70c1895198fd24fec1db3a6319f0a0ab9a668a25
Fernando stteffen <fernando.stteffen-ext@ab-inbev.com>
2023-02-07 09:32:46 -0300

    feat: start project dart

---
+++ bin/dart_collections_funcs.dart
@@ -0,0 +1,5 @@
+import 'package:dart_collections_funcs/dart_collections_funcs.dart' as dart_collections_funcs;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_collections_funcs.calculate()}!');
+}
